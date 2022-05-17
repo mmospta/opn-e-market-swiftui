0f426625aa4dc5e0228f4e99466a534fb64d2f43
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-05-17 20:44:44 +0700

    e-market: initial project

---
+++ e-market/e-market/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  e-market
+//
+//  Created by Phonthep Aungkanukulwit on 17/5/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ e-market/e-market/e_marketApp.swift
@@ -0,0 +1,17 @@
+//
+//  e_marketApp.swift
+//  e-market
+//
+//  Created by Phonthep Aungkanukulwit on 17/5/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct e_marketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
