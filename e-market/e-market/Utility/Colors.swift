3d44b19abc04dc8cbfe804da329804c4b105b0c4
Phonthep Aungkanukulwit <phonthep.aungkanukulwit@scbtechx.io>
2022-05-18 03:07:17 +0700

    e-market: add image and color

---
+++ e-market/e-market/Utility/Colors.swift
@@ -0,0 +1,25 @@
+//
+//  Colors.swift
+//  e-market
+//
+//  Created by Phonthep Aungkanukulwit on 18/5/2565 BE.
+//
+
+import Foundation
+import SwiftUI
+
+struct Style {
+  struct Colors {
+    static let background = Color.init(hex: "#F2F2F2")
+    static let grayBackground = Color.init(hex: "#DDDDDD")
+    static let gray = Color.init(hex: "#F6F6F6")
+    static let grayb = Color.init(hex: "#989898")
+    static let grayc = Color.init(hex: "#C9C9C9")
+    static let dark = Color.init(hex: "#2D2D2D")
+    static let textBlack = Color.init(hex: "#090909")
+    static let textGray = Color.init(hex: "#B0B0B0")
+    static let textLightGray = Color.init(hex: "#C8C8C8")
+    static let textGold = Color.init(hex: "#C9AD96")
+    static let textBrown = Color.init(hex: "#BEA084")
+  }
+}
